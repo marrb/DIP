54963415af0ea9e60e80ee60d3777a7a82d147c8
marrb <martin.bublavy@gmail.com>
2025-03-20 23:08:56 +0100

    add taiwlind to user study FE

--- user_study_fe/app.vue
+++ user_study_fe/app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="bg-red-500">
+    Zoopla
   </div>
 </template>
--- user_study_fe/nuxt.config.ts
+++ user_study_fe/nuxt.config.ts
@@ -1,5 +1,19 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+	compatibilityDate: "2024-11-01",
+	css: ["~/assets/css/main.css"],
+	vite: {
+		plugins: [tailwindcss()],
+		build: {
+			minify: true,
+		},
+	},
+	nitro: {
+		preset: "node-server",
+		compressPublicAssets: true,
+	},
+	devtools: { enabled: true },
+	ssr: true,
+});
