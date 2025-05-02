4516dfa4d62810c1c64d5d34843c8a8b40ae067f
marrb <martin.bublavy@gmail.com>
2025-05-02 17:36:02 +0200

    add processing for video questions

--- data_processing/user_study_results_processing/QuestionProcessor.py
+++ data_processing/user_study_results_processing/QuestionProcessor.py
@@ -4,6 +4,7 @@ from json_types.QuestionJsonData import QuestionJsonData
 from json_types.EQuestionType import EQuestionType
 from json_types.Question import Question
 from json_types.Answer import Answer
+from json_types.Video import Video
 from typing import List, Tuple
 
 class QuestionProcessor:
@@ -35,5 +36,33 @@ class QuestionProcessor:
             question_answer_pairs.append((question, answers))
             
         return question_answer_pairs
+    
+    def get_question_by_id_and_answers(self, id: str) -> Tuple[Question, List[Answer]]:
+        question = next((q for q in self.data['questions'] if q['id'] == id), None)
         
+        if question is None:
+            raise ValueError(f"Question with id {id} not found.")
+        
+        answers = [
+            a 
+            for user_answer in self.data['answers']
+            for a in user_answer['data']
+            if a['questionId'] == id
+        ]
+        
+        return question, answers
+        
+    def get_videos_by_question_id(self, question_id: str) -> List[Video]:
+        question = next((q for q in self.data['questions'] if q['id'] == question_id), None)
+        
+        if question is None:
+            raise ValueError(f"Question with id {question_id} not found.")
+        
+        videos = [
+            v 
+            for v in self.data['videos'] 
+            if v['id'] in [qv['id'] for qv in question['videos']]
+        ]
+        
+        return videos
         
